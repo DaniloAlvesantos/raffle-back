ca822178c3077458480f87d3d219604b35d821ca
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2024-02-12 09:46:54 -0300

    serverless with routes

--- api/serveless.ts
+++ api/serveless.ts
@@ -1,22 +1,30 @@
 "use strict";
 
 import * as dotenv from "dotenv";
-import routes from "../src/server";
 dotenv.config();
 
 // Require the framework
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 
-// Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
 });
 
-// Register your application as a normal plugin.
-app.register(routes, {
-  prefix: "/",
+// Importe as rotas necessárias aqui
+import { RifaRoutes } from "../src/routes/rifa";
+import { NumbersRoutes } from "../src/routes/numbers";
+import { AuthRoutes } from "../src/routes/auth";
+import jwt from "@fastify/jwt";
+
+// Registrar middleware de autenticação JWT
+app.register(jwt, {
+  secret: "waypremios13121123312",
 });
 
+// Registrar todas as rotas
+app.register(RifaRoutes);
+app.register(NumbersRoutes);
+app.register(AuthRoutes);
 export default async (req: FastifyRequest, res: FastifyReply) => {
   await app.ready();
   app.server.emit("request", req, res);
--- src/server.ts
+++ src/server.ts
@@ -3,35 +3,26 @@ import cors from "@fastify/cors";
 import { RifaRoutes } from "./routes/rifa";
 import { NumbersRoutes } from "./routes/numbers";
 import { AuthRoutes } from "./routes/auth";
-import jwt from "@fastify/jwt"
+import jwt from "@fastify/jwt";
 
 export default async function bootStrap() {
-  const port = process.env.PORT || 33333  
-
   const fastify = Fastify({
     logger: true,
   });
 
   await fastify.register(cors, {
-    origin:true,
-  })
+    origin: true,
+  });
 
-  fastify.register(RifaRoutes)
-  fastify.register(NumbersRoutes)
-  fastify.register(AuthRoutes)
+  fastify.register(RifaRoutes);
+  fastify.register(NumbersRoutes);
+  fastify.register(AuthRoutes);
 
   await fastify.register(jwt, {
-    secret: 'waypremios13121123312',
-  })
+    secret: "waypremios13121123312",
+  });
 
-  try {
-    await fastify.listen(port);
-  } catch(err) {
-    console.log(err)
-    process.exit(1);
-  }
+  await fastify.listen({ port: 33333 });
 }
 
 bootStrap();
-
-export { bootStrap }
